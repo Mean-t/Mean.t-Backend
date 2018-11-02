00c47a3e3682dcd2b0550aea81cbeb9cf86cc0ac
Lewis <artoria@artoria.us>
2018-11-02 20:52:14 +0900

    REMOVE: unused redis and mongodb settings

--- Server/config/__init__.py
+++ Server/config/__init__.py
@@ -13,26 +13,11 @@ class Config:
 
     SECRET_KEY = os.getenv('SECRET_KEY', '85c145a16bd6f6e1f3e104ca78c6a102')
 
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=30)
-    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=365)
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=1)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
     JWT_HEADER_TYPE = 'JWT'
 
-    MONGODB_SETTINGS = {
-        'host': None,
-        'port': None,
-        'username': None,
-        'password': os.getenv('MONGO_PW_{}'.format(SERVICE_NAME_UPPER)),
-        'db': SERVICE_NAME
-    }
-
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:wasitacatisaw?@localhost:3333/meant"
-
-    REDIS_SETTINGS = {
-        'host': 'localhost',
-        'port': 6379,
-        'password': os.getenv('REDIS_PW_{}'.format(SERVICE_NAME_UPPER)),
-        'db': 0
-    }
 
     SWAGGER = {
         'title': SERVICE_NAME,
