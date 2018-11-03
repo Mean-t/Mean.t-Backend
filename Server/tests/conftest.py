7d751b498e2ee522ccac898fcdaa387536bd0e1f
Lewis <artoria@artoria.us>
2018-11-03 18:54:32 +0900

    ADD: logger to session test fixtures

--- Server/tests/conftest.py
+++ Server/tests/conftest.py
@@ -8,11 +8,13 @@ from alembic.config import Config as AlembicConfig
 
 from app import create_app
 from config.test import TestConfig  # flask application configuration
+from app.misc.logger import logger
 
 # typing hits
 from flask import Flask
 from flask.testing import FlaskClient
 from sqlalchemy.engine.base import Engine
+from sqlalchemy.orm import Session
 from typing import Dict
 
 
@@ -22,13 +24,16 @@ def flask_app() -> Flask:
     app_context = app.app_context()
     app_context.push()
 
+    logger(message="flask app created", type="INFO")
     yield app
 
+    logger(message="flask app released", type="INFO")
     app_context.pop()
 
 
 @pytest.fixture(scope="session")
 def flask_client(flask_app: Flask) -> FlaskClient:
+    logger(message="flask test client created", type="INFO")
     return flask_app.test_client()
 
 
@@ -36,7 +41,7 @@ def flask_client(flask_app: Flask) -> FlaskClient:
 def db() -> Dict[Engine, sessionmaker]:
     engine = create_engine(TestConfig.SQLALCHEMY_DATABASE_URI, echo=True)
     session = sessionmaker(bind=engine)
-    print(type(engine), type(session))
+
     _db = {
         'engine': engine,
         'session': session
@@ -47,17 +52,21 @@ def db() -> Dict[Engine, sessionmaker]:
     alembic_config.set_main_option('sqlalchemy.url', TestConfig.SQLALCHEMY_DATABASE_URI)
     alembic_upgrade(alembic_config, 'head')
 
+    logger(message="database created", type="INFO")
     yield _db
 
+    logger(message="database disposed", type="INFO")
     engine.dispose()
 
 
 @pytest.fixture(scope='function')
-def session(db: Dict[Engine, sessionmaker]) -> sessionmaker:
+def session(db: Dict[Engine, sessionmaker]) -> Session:
     session = db['session']()
     g.db = session
 
+    logger(message="database session created", type="INFO")
     yield session
 
+    logger(message="database session closed", type="INFO")
     session.rollback()
     session.close()
