9f00f5bbb6615bbfee89373f705329d1d4903c1c
Lewis <artoria@artoria.us>
2018-11-04 14:04:22 +0900

    UPDATE: add id columns. status enums and new funding_status table

--- SQL/ddl.sql
+++ SQL/ddl.sql
@@ -4,64 +4,92 @@ use meant;
 
 create table funding
 (
-  email           varchar(50)   not null,
-  code            varchar(10)   not null,
-  title           varchar(45)   null,
-  body            varchar(1600) null,
-  expiration      datetime      null,
-  goal            int           null,
-  title_img_path  varchar(100)  null,
-  cover_img_path  varchar(100)  null,
-  header_img_path varchar(500)  null,
-  host            varchar(10)   not null,
-  funding_id      int           not null,
-  primary key (email, code)
+  funding_id       int unsigned                       not null,
+  email            varchar(50)                        not null,
+  code             varchar(10)                        not null,
+  title            varchar(45)                        null,
+  body             varchar(1600)                      null,
+  expiration       datetime                           null,
+  title_img_path   varchar(100)                       null,
+  cover_img_path   varchar(100)                       null,
+  header_img_paths varchar(500)                       null,
+  host             varchar(10)                        null,
+  created_at       datetime default CURRENT_TIMESTAMP not null,
+  updated_at       datetime default CURRENT_TIMESTAMP not null,
+  primary key (funding_id, email, code),
+  constraint index_UNIQUE
+  unique (funding_id)
+);
+
+create table funding_status
+(
+  funding_funding_id int unsigned                                         not null,
+  funding_email      varchar(50)                                          not null,
+  funding_code       varchar(10)                                          not null,
+  balance            int unsigned default '0'                             not null,
+  participants_num   int unsigned default '0'                             not null,
+  status             enum ('ready', 'success', 'manufacture', 'complete') null,
+  primary key (funding_funding_id, funding_email, funding_code),
+  constraint fk_funding_status_funding1
+  foreign key (funding_funding_id, funding_email, funding_code) references funding (funding_id, email, code)
+    on update cascade
+    on delete cascade
 );
 
 create table idea
 (
-  email   varchar(50)   not null,
-  code    varchar(10)   not null,
-  title   varchar(45)   null,
-  body    varchar(1600) null,
-  idea_id int           not null,
-  primary key (email, code)
+  idea_id    int unsigned auto_increment,
+  email      varchar(50)                        not null,
+  code       varchar(10)                        not null,
+  title      varchar(45)                        null,
+  body       varchar(1600)                      null,
+  created_at datetime default CURRENT_TIMESTAMP not null,
+  updated_at datetime default CURRENT_TIMESTAMP not null,
+  primary key (idea_id, email, code)
 );
 
-create table idea_has_funding
+create table funding_has_idea
 (
-  idea_email    varchar(50) not null,
-  idea_code     varchar(10) not null,
-  funding_email varchar(50) not null,
-  funding_code  varchar(10) not null,
-  primary key (idea_email, idea_code, funding_email, funding_code),
-  constraint fk_idea_has_funding_funding1
-  foreign key (funding_email, funding_code) references funding (email, code),
-  constraint fk_idea_has_funding_idea1
-  foreign key (idea_email, idea_code) references idea (email, code)
+  funding_funding_id int unsigned not null,
+  funding_email      varchar(50)  not null,
+  funding_code       varchar(10)  not null,
+  idea_idea_id       int unsigned not null,
+  idea_email         varchar(50)  not null,
+  idea_code          varchar(10)  not null,
+  primary key (funding_funding_id, funding_email, funding_code, idea_idea_id, idea_email, idea_code),
+  constraint fk_funding_has_idea_funding1
+  foreign key (funding_funding_id, funding_email, funding_code) references funding (funding_id, email, code)
+    on update cascade
+    on delete cascade,
+  constraint fk_funding_has_idea_idea1
+  foreign key (idea_idea_id, idea_email, idea_code) references idea (idea_id, email, code)
+    on update cascade
 );
 
-create index fk_idea_has_funding_funding1_idx
-  on idea_has_funding (funding_email, funding_code);
+create index fk_funding_has_idea_funding1_idx
+  on funding_has_idea (funding_funding_id, funding_email, funding_code);
 
-create index fk_idea_has_funding_idea1_idx
-  on idea_has_funding (idea_email, idea_code);
+create index fk_funding_has_idea_idea1_idx
+  on funding_has_idea (idea_idea_id, idea_email, idea_code);
 
 create table `order`
 (
-  code          varchar(10) not null,
-  email         varchar(50) not null,
-  funding_email varchar(50) not null,
-  funding_code  varchar(10) not null,
-  payee         varchar(10) not null,
-  destination   varchar(50) not null,
-  primary key (code, email, funding_email, funding_code),
+  code               varchar(10)                                                                                not null,
+  email              varchar(50)                                                                                not null,
+  payee              varchar(10)                                                                                not null,
+  destination        varchar(50)                                                                                not null,
+  status             enum ('cancel', 'standby', 'ready', 'manufacture', 'delivery', 'arrive') default 'standby' not null,
+  ordered_at         datetime default CURRENT_TIMESTAMP                                                         not null,
+  funding_funding_id int unsigned                                                                               not null,
+  funding_email      varchar(50)                                                                                not null,
+  funding_code       varchar(10)                                                                                not null,
+  primary key (code, email, funding_funding_id, funding_email, funding_code),
   constraint fk_order_funding1
-  foreign key (funding_email, funding_code) references funding (email, code)
+  foreign key (funding_funding_id, funding_email, funding_code) references funding (funding_id, email, code)
 );
 
 create index fk_order_funding1_idx
-  on `order` (funding_email, funding_code);
+  on `order` (funding_funding_id, funding_email, funding_code);
 
 create table tag
 (
@@ -69,45 +97,43 @@ create table tag
     primary key
 );
 
+create table funding_has_tag
+(
+  funding_funding_id int unsigned not null,
+  funding_email      varchar(50)  not null,
+  funding_code       varchar(10)  not null,
+  tag_title          varchar(45)  not null,
+  primary key (funding_funding_id, funding_email, funding_code, tag_title),
+  constraint fk_funding_has_tag_funding1
+  foreign key (funding_funding_id, funding_email, funding_code) references funding (funding_id, email, code)
+    on update cascade,
+  constraint fk_funding_has_tag_tag1
+  foreign key (tag_title) references tag (title)
+);
+
+create index fk_funding_has_tag_funding1_idx
+  on funding_has_tag (funding_funding_id, funding_email, funding_code);
+
+create index fk_funding_has_tag_tag1_idx
+  on funding_has_tag (tag_title);
+
 create table idea_has_tag
 (
-  idea_email varchar(50) not null,
-  idea_code  varchar(10) not null,
-  tag_title  varchar(45) not null,
-  primary key (idea_email, idea_code, tag_title),
+  idea_idea_id int unsigned not null,
+  idea_email   varchar(50)  not null,
+  idea_code    varchar(10)  not null,
+  tag_title    varchar(45)  not null,
+  primary key (idea_idea_id, idea_email, idea_code, tag_title),
   constraint fk_idea_has_tag_idea1
-  foreign key (idea_email, idea_code) references idea (email, code)
+  foreign key (idea_idea_id, idea_email, idea_code) references idea (idea_id, email, code)
     on update cascade,
   constraint fk_idea_has_tag_tag1
   foreign key (tag_title) references tag (title)
-    on update cascade
-    on delete cascade
 );
 
 create index fk_idea_has_tag_idea1_idx
-  on idea_has_tag (idea_email, idea_code);
+  on idea_has_tag (idea_idea_id, idea_email, idea_code);
 
 create index fk_idea_has_tag_tag1_idx
   on idea_has_tag (tag_title);
-
-create table tag_has_funding
-(
-  tag_title     varchar(45) not null,
-  funding_email varchar(50) not null,
-  funding_code  varchar(10) not null,
-  primary key (tag_title, funding_email, funding_code),
-  constraint fk_tag_has_funding_funding1
-  foreign key (funding_email, funding_code) references funding (email, code)
-    on update cascade
-    on delete cascade,
-  constraint fk_tag_has_funding_tag1
-  foreign key (tag_title) references tag (title)
-    on update cascade
-);
-
-create index fk_tag_has_funding_funding1_idx
-  on tag_has_funding (funding_email, funding_code);
-
-create index fk_tag_has_funding_tag1_idx
-  on tag_has_funding (tag_title);
 
--- Server/app/models/__init__.py
+++ Server/app/models/__init__.py
@@ -1,94 +1,147 @@
-from typing import List, Tuple
+from __future__ import annotations
+
+from typing import List, Tuple, Type
 from datetime import datetime
-from sqlalchemy import Table, ForeignKeyConstraint, Index
+from sqlalchemy import ForeignKeyConstraint, Table, Index
+from enum import Enum
+
 from app.extensions import db
 
 
-class Tag(db.Model):
-    __tablename__ = 'tag'
+class FundingStatusEnum(Enum):
+    ready = 1
+    success = 2
+    manufacture = 3
+    complete = 4
 
-    title: str = db.Column(db.String(45), primary_key=True)
 
-
-t_tag_has_funding: Table = db.Table(
-    'tag_has_funding',
-    db.Column('tag_title', db.ForeignKey('tag.title', onupdate='CASCADE'),
-              primary_key=True, nullable=False, index=True),
-    db.Column('funding_email', db.String(50), primary_key=True, nullable=False),
-    db.Column('funding_code', db.String(10), primary_key=True, nullable=False),
-    db.ForeignKeyConstraint(('funding_email', 'funding_code'), ['funding.email', 'funding.code'],
-                            ondelete='CASCADE', onupdate='CASCADE'),
-    db.Index('fk_tag_has_funding_funding1_idx', 'funding_email', 'funding_code')
-)
-
-t_idea_has_tag: Table = db.Table(
-    'idea_has_tag',
-    db.Column('idea_email', db.String(50), primary_key=True, nullable=False),
-    db.Column('idea_code', db.String(10), primary_key=True, nullable=False),
-    db.Column('tag_title', db.ForeignKey('tag.title', ondelete='CASCADE', onupdate='CASCADE'),
-              primary_key=True, nullable=False, index=True),
-    db.ForeignKeyConstraint(('idea_email', 'idea_code'), ['idea.email', 'idea.code'], onupdate='CASCADE'),
-    db.Index('fk_idea_has_tag_idea1_idx', 'idea_email', 'idea_code')
-)
+class OrderStatusEnum(Enum):
+    cancel = 1
+    standby = 2
+    ready = 3
+    manufacture = 4
+    delivery = 5
+    arrive = 6
 
 
 class Funding(db.Model):
     __tablename__ = 'funding'
 
-    funding_id = db.Column(db.Integer, nullable=False)
+    funding_id: int = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
     email: str = db.Column(db.String(50), primary_key=True, nullable=False)
-    host: str = db.Column(db.String(10), nullable=False)
     code: str = db.Column(db.String(10), primary_key=True, nullable=False)
     title: str = db.Column(db.String(45))
     body: str = db.Column(db.String(1600))
     expiration: datetime = db.Column(db.DateTime)
     title_img_path: str = db.Column(db.String(100))
     cover_img_path: str = db.Column(db.String(100))
-    header_img_path: str = db.Column(db.String(500))
+    header_img_paths: str = db.Column(db.String(500))
+    host: str = db.Column(db.String(10))
+    created_at: datetime = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue())
+    updated_at: datetime = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue())
 
-    tag = db.relationship('Tag', secondary='tag_has_funding', backref='fundings')  # type: List[Tag]
+    idea_ideas: List[Idea] = db.relationship('Idea', secondary='funding_has_idea', backref='fundings')
+    tag: List[Tag] = db.relationship('Tag', secondary='funding_has_tag', backref='fundings')
+
+
+class FundingStatus(Funding):
+    __tablename__ = 'funding_status'
+    __table_args__: Tuple[ForeignKeyConstraint] = (
+        db.ForeignKeyConstraint(('funding_funding_id', 'funding_email', 'funding_code'),
+                                ['funding.funding_id', 'funding.email', 'funding.code'],
+                                ondelete='CASCADE', onupdate='CASCADE'),
+    )
+
+    funding_funding_id: int = db.Column(db.Integer, primary_key=True, nullable=False)
+    funding_email: str = db.Column(db.String(50), primary_key=True, nullable=False)
+    funding_code: str = db.Column(db.String(10), primary_key=True, nullable=False)
+    balance: int = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue())
+    participants_num: int = db.Column(db.Integer, nullable=False, server_default=db.FetchedValue())
+    status: Type[Enum] = db.Column(db.Enum(FundingStatusEnum))
+
+
+t_funding_has_idea: Table = db.Table(
+    'funding_has_idea',
+    db.Column('funding_funding_id', db.Integer, primary_key=True, nullable=False),
+    db.Column('funding_email', db.String(50), primary_key=True, nullable=False),
+    db.Column('funding_code', db.String(10), primary_key=True, nullable=False),
+    db.Column('idea_idea_id', db.Integer, primary_key=True, nullable=False),
+    db.Column('idea_email', db.String(50), primary_key=True, nullable=False),
+    db.Column('idea_code', db.String(10), primary_key=True, nullable=False),
+    db.ForeignKeyConstraint(('funding_funding_id', 'funding_email', 'funding_code'),
+                            ['funding.funding_id', 'funding.email', 'funding.code'],
+                            ondelete='CASCADE', onupdate='CASCADE'),
+    db.ForeignKeyConstraint(('idea_idea_id', 'idea_email', 'idea_code'),
+                            ['idea.idea_id', 'idea.email', 'idea.code'],
+                            onupdate='CASCADE'),
+    db.Index('fk_funding_has_idea_idea1_idx', 'idea_idea_id', 'idea_email', 'idea_code'),
+    db.Index('fk_funding_has_idea_funding1_idx', 'funding_funding_id', 'funding_email', 'funding_code')
+)
+
+t_funding_has_tag: Table = db.Table(
+    'funding_has_tag',
+    db.Column('funding_funding_id', db.Integer, primary_key=True, nullable=False),
+    db.Column('funding_email', db.String(50), primary_key=True, nullable=False),
+    db.Column('funding_code', db.String(10), primary_key=True, nullable=False),
+    db.Column('tag_title', db.ForeignKey('tag.title'), primary_key=True, nullable=False, index=True),
+    db.ForeignKeyConstraint(('funding_funding_id', 'funding_email', 'funding_code'),
+                            ['funding.funding_id', 'funding.email', 'funding.code'], onupdate='CASCADE'),
+    db.Index('fk_funding_has_tag_funding1_idx', 'funding_funding_id', 'funding_email', 'funding_code')
+)
 
 
 class Idea(db.Model):
     __tablename__ = 'idea'
 
-    idea_id = db.Column(db.Integer, nullable=False)
+    idea_id: int = db.Column(db.Integer, primary_key=True, nullable=False)
     email: str = db.Column(db.String(50), primary_key=True, nullable=False)
     code: str = db.Column(db.String(10), primary_key=True, nullable=False)
     title: str = db.Column(db.String(45))
     body: str = db.Column(db.String(1600))
+    created_at: datetime = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue())
+    updated_at: datetime = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue())
 
     tag: List[Tag] = db.relationship('Tag', secondary='idea_has_tag', backref='ideas')
 
 
-t_idea_has_funding: Table = db.Table(
-    'idea_has_funding',
+t_idea_has_ta: Table = db.Table(
+    'idea_has_tag',
+    db.Column('idea_idea_id', db.Integer, primary_key=True, nullable=False),
     db.Column('idea_email', db.String(50), primary_key=True, nullable=False),
     db.Column('idea_code', db.String(10), primary_key=True, nullable=False),
-    db.Column('funding_email', db.String(50), primary_key=True, nullable=False),
-    db.Column('funding_code', db.String(10), primary_key=True, nullable=False),
-    db.ForeignKeyConstraint(('funding_email', 'funding_code'), ['funding.email', 'funding.code']),
-    db.ForeignKeyConstraint(('idea_email', 'idea_code'), ['idea.email', 'idea.code']),
-    db.Index('fk_idea_has_funding_idea1_idx', 'idea_email', 'idea_code'),
-    db.Index('fk_idea_has_funding_funding1_idx', 'funding_email', 'funding_code')
+    db.Column('tag_title', db.ForeignKey('tag.title'), primary_key=True, nullable=False, index=True),
+    db.ForeignKeyConstraint(('idea_idea_id', 'idea_email', 'idea_code'),
+                            ['idea.idea_id', 'idea.email', 'idea.code'],
+                            onupdate='CASCADE'),
+    db.Index('fk_idea_has_tag_idea1_idx', 'idea_idea_id', 'idea_email', 'idea_code')
 )
 
 
 class Order(db.Model):
     __tablename__ = 'order'
     __table_args__: Tuple[ForeignKeyConstraint, Index] = (
-        db.ForeignKeyConstraint(('funding_email', 'funding_code'), ['funding.email', 'funding.code']),
-        db.Index('fk_order_funding1_idx', 'funding_email', 'funding_code')
+        db.ForeignKeyConstraint(('funding_funding_id', 'funding_email', 'funding_code'),
+                                ['funding.funding_id', 'funding.email', 'funding.code']),
+        db.Index('fk_order_funding1_idx', 'funding_funding_id', 'funding_email', 'funding_code')
     )
 
     code: str = db.Column(db.String(10), primary_key=True, nullable=False)
     email: str = db.Column(db.String(50), primary_key=True, nullable=False)
     payee: str = db.Column(db.String(10), nullable=False)
     destination: str = db.Column(db.String(50), nullable=False)
+    status: Type[Enum] = db.Column(db.Enum(OrderStatusEnum), nullable=False, server_default=db.FetchedValue())
+    ordered_at: str = db.Column(db.DateTime, nullable=False, server_default=db.FetchedValue())
+    funding_funding_id: int = db.Column(db.Integer, primary_key=True, nullable=False)
     funding_email: str = db.Column(db.String(50), primary_key=True, nullable=False)
-    funding_code = db.Column(db.String(10), primary_key=True, nullable=False)
+    funding_code: str = db.Column(db.String(10), primary_key=True, nullable=False)
 
-    funding: Funding = db.relationship('Funding',
-                                       primaryjoin='and_(Order.funding_email == Funding.email, \
-                                          Order.funding_code == Funding.code)',
-                                       backref='orders')
+    funding_funding: Funding = db.relationship('Funding',
+                                               primaryjoin='and_(Order.funding_funding_id == Funding.funding_id, '
+                                                           'Order.funding_email == Funding.email, '
+                                                           'Order.funding_code == Funding.code)', backref='orders')
+
+
+class Tag(db.Model):
+    __tablename__ = 'tag'
+
+    title: str = db.Column(db.String(45), primary_key=True)
