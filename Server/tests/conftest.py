b845b8a67ce3ea2924145bea3075d637e6ba3a0d
Lewis <artoria@artoria.us>
2018-11-04 21:57:10 +0900

    UPDATE: change order of delete from statement for avoid fk constraint

--- Server/tests/conftest.py
+++ Server/tests/conftest.py
@@ -77,7 +77,7 @@ def session(db: Dict[Engine, sessionmaker]) -> Session:
     logger(message="database session closed", keyword="INFO")
     session.rollback()
     i: str
-    for i in ["funding", "idea", "tag", "order", "funding_status",
-              "idea_has_tag", "funding_has_tag", "funding_has_idea"]:
+    for i in ["idea_has_tag", "funding_has_tag", "funding_has_idea",
+              "tag", "funding", "idea", "order", "funding_status", ]:
         session.execute('DELETE FROM meant.{}'.format(i))
     session.close()
