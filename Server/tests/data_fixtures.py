552480945201b6965c39e9bfb44a05434769eb61
Lewis <artoria@artoria.us>
2018-11-04 20:50:26 +0900

    ADD: tag data and commit code

--- Server/tests/data_fixtures.py
+++ Server/tests/data_fixtures.py
@@ -1,16 +1,21 @@
 import pytest
 import uuid
 
-from app.models import Idea
+from app.models import Idea, Tag
 
 
 @pytest.fixture(scope="function")
 def idea(session):
+    new_tag = Tag(title="raincoat")
+    session.add(new_tag)
+
     new_idea = Idea(email="artoria@artoria.us",
                     code=str(uuid.uuid4()).upper().replace("-", "")[:10],
                     title="raincoat",
+                    tag=[new_tag],
                     body="I have an idea about raincoat")
 
     session.add(new_idea)
+    session.commit()
 
     return new_idea
