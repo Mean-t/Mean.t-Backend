b3d39440cf6e4d424396ad055b0ff53406fc2fe2
Lewis <artoria@artoria.us>
2018-11-05 13:24:07 +0900

    ADD: funding data fixture

--- Server/tests/data_fixtures.py
+++ Server/tests/data_fixtures.py
@@ -1,7 +1,8 @@
 import pytest
 import uuid
+from datetime import datetime, timedelta
 
-from app.models import Idea, Tag
+from app.models import Idea, Tag, Funding
 
 
 @pytest.fixture(scope="function")
@@ -19,3 +20,24 @@ def idea(session):
     session.commit()
 
     return new_idea
+
+
+@pytest.fixture(scope="function")
+def funding(session, idea):
+    new_funding: Funding = Funding(email="artoria@artoria.us",
+                                   host="lewis kim",
+                                   code=str(uuid.uuid4()).upper().replace("-", "")[:10],
+                                   title="bulletproof raincoat",
+                                   tag=[idea.tag],
+                                   body="ability of withstand .50 BMG",
+                                   expiration=datetime.date(datetime.now() + timedelta(days=10)),
+                                   goal=1000000000,
+                                   idea_ideas=[idea],
+                                   title_img_path="/static/wef2r243.jpg",
+                                   cover_img_path="/static/wef2r243.jpg",
+                                   header_img_paths="/static/wef2r243.jpg\\/static/wef2r243.jpg\\/static/wef2r243.jpg"
+                                   )
+    session.add(new_funding)
+    session.commit()
+
+    return new_funding
