df1cd0acb570a7cf91613371743a879e02c8626a
Lewis <artoria@artoria.us>
2018-12-06 17:02:09 +0900

    concat: funding view and search TC

--- Server/tests/views/test_view_funding.py
+++ Server/tests/views/test_view_funding.py
@@ -9,18 +9,13 @@ from flask import Response
 from typing import Dict, List
 
 
-class TestViewFunding:
-    def test_view_funding_list(self, flask_client, funding) -> None:
+class TestSearch:
+    def test_funding(self, flask_client, funding) -> None:
         res: Response = flask_client.get("/api/v1/funding")
 
         # default response check
         assert "application/json" == res.headers["Content-Type"]
-        assert 201 == res.status_code
-
-        # HATEOAS check
-        links: Dict[str] = res.data["links"]
-
-        assert "/api/v1/funding" == links["funding"]
+        assert 200 == res.status_code
 
         # data check
         assert isinstance(list, res.data["fundings"])
@@ -29,10 +24,9 @@ class TestViewFunding:
         funding_list: List[Dict[str]] = res.data["fundings"]
         funding_instance: Dict[str] = funding_list[0]
 
-        assert "artoria@artoria.us" == funding_instance["email"]
-        assert "raincoat" == funding_instance["title"]
-        assert isinstance(funding_instance["tag"], list)
-        assert "raincoat" == funding_instance["tag"][0]
+        assert funding_instance["email"] == funding.email
+        assert funding_instance["title"] == funding.title
+        assert funding_instance["tag"] == [tag.title for tag in funding.tag]
 
         # instance HATEOAS check
         assert isinstance(funding_instance["links"], dict)
@@ -42,32 +36,31 @@ class TestViewFunding:
         link_regex = re.compile(r"[/]api[/]v1[/]funding[/]\d")
         assert re.match(link_regex, links["self"])
 
-        self.funding_instance_link: str = links["self"]
-
-    def test_view_specific_funding(self, flask_client, funding: Funding) -> None:
-        res: Response = flask_client.get('/api/v1/funding/{}'.format(funding.funding_id))
+    def test_view_specific_funding(self, flask_client, funding) -> None:
+        funding_instance_link: str = flask_client.get("/api/v1/funding").data["fundings"][0]["links"]["self"]
+        res: Response = flask_client.get(funding_instance_link)
 
         # default response check
         assert "application/json" == res.headers["Content-Type"]
-        assert 201 == res.status_code
+        assert 200 == res.status_code
 
         # HATEOAS check
         links: Dict[str] = res.data["links"]
 
-        assert self.funding_instance_link == links["self"]
-        assert self.funding_instance_link + "/order" == links["order"]
+        assert funding_instance_link == links["self"]
+        assert funding_instance_link + "/order/verify" == links["order"]
 
         # data check
         assert isinstance(dict, res.data)
 
-        res_data: Dict[str] = res.data
+        funding_data: Dict[str] = res.data
 
-        assert funding.email == res_data["email"]
-        assert funding.host == res_data["host"]
-        assert funding.title == res_data["title"]
-        assert isinstance(res_data["tag"], list)
-        assert [tag.title for tag in funding.tag] == res_data["tag"]
-        assert funding.body == res_data["body"]
-        assert funding.title_img_path == res_data["title_image"]
-        assert funding.cover_img_path == res_data["cover_image"]
-        assert funding.header_img_paths.split("%") == res_data["header_image_paths"]
+        assert funding.email == funding_data["email"]
+        assert funding.host == funding_data["host"]
+        assert funding.title == funding_data["title"]
+        assert isinstance(funding_data["tag"], list)
+        assert [tag.title for tag in funding.tag] == funding_data["tag"]
+        assert funding.body == funding_data["body"]
+        assert funding.title_img_path == funding_data["title_image"]
+        assert funding.cover_img_path == funding_data["cover_image"]
+        assert funding.header_img_paths.split("%") == funding_data["header_image_paths"]
