e304024aafdf33de94897899e4fd446706c879ac
Lewis <artoria@artoria.us>
2018-12-12 19:16:22 +0900

    restructure: view classes

--- Server/app/views/order/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import Api
-
-from app.blueprints import api_v1_blueprint
-from app.views.order import inquiry
-
-api_order = Api(api_v1_blueprint, prefix='/order')
-
-api_order.add_resource(inquiry.Order, '')
